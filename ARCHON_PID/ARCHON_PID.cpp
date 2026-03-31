cb0b7f8074ca6763db2b08106ac47c25f8211030
GianmarcoBC <g.barcelloni-corte@alumnos.upm.es>
2026-03-31 22:20:21 +0200

    Movimiento de Resources a bin (directorio de trabajo)
    los archivos que deban utilizarse en el código deben estar dentro de la carpeta bin del proyecto.
    Añado pequeño ejemplo en el main para el uso de musica, se puede eliminar si es necesario.

--- ARCHON_PID/ARCHON_PID.cpp
+++ ARCHON_PID/ARCHON_PID.cpp
@@ -1,17 +1,29 @@
-#include "raylib.h"
+#include <raylib.h>
 
 int main()
 {
     InitWindow(800, 600, "Test Raylib");
     SetTargetFPS(60);
+    InitAudioDevice();
+    
+    Music musica = LoadMusicStream("Resources/Audio/Musica/MusciaTableroFin.mp3");  // archivo en bin/
+    PlayMusicStream(musica);
 
     while (!WindowShouldClose()) {
+        UpdateMusicStream(musica);  // necesario llamarlo cada frame
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Raylib funciona!", 300, 280, 20, DARKGRAY);
+        if (musica.stream.buffer == NULL)
+            DrawText("Error cargando musica", 300, 260, 20, RED);
+        else
+            DrawText("Musica cargada OK", 300, 260, 20, GREEN);
         EndDrawing();
     }
 
+    UnloadMusicStream(musica);
+    CloseAudioDevice();
     CloseWindow();
     return 0;
 }
