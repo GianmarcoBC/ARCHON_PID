774e4732a2692f9090f5bf7350b87bc6d6d59563
JulianJH22 <166717645+JulianJH22@users.noreply.github.com>
2026-04-03 23:44:50 +0200

    Add files via upload

--- ARCHON_PID/src/ARCHON_PID.cpp
+++ ARCHON_PID/src/ARCHON_PID.cpp
@@ -1,28 +1,41 @@
 #include <raylib.h>
+#include "Archer.h"
 
 int main()
 {
-    InitWindow(800, 600, "Test Raylib");
+    
+    InitWindow(1600, 800, "Test Raylib");
     SetTargetFPS(60);
     InitAudioDevice();
-    
+
+    Archer A1;
     Music musica = LoadMusicStream("bin/Resources/Audio/Musica/MusicaBatalla1V1.mp3");  // archivo en bin/
+    //Texture2D Sprite = LoadTexture("bin/Resources/Graficos/Sprites/26-267686_tortugas-ninja-png-imagenes-de-tortugas-ninja-png.png");
     PlayMusicStream(musica);
+
 
     while (!WindowShouldClose()) {
         UpdateMusicStream(musica);  // necesario llamarlo cada frame
-
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Raylib funciona!", 300, 280, 20, DARKGRAY);
+
         if (musica.stream.buffer == NULL)
             DrawText("Error cargando musica", 300, 260, 20, RED);
         else
             DrawText("Musica cargada OK", 300, 260, 20, GREEN);
+
+        if (A1.Sprite.id != 0)
+            DrawTexture(A1.Sprite, 200, 100, WHITE);
+        else
+            DrawText("Error cargando imagen", 300, 240, 20, RED);
+
+
         EndDrawing();
     }
 
     UnloadMusicStream(musica);
+	UnloadTexture(A1.Sprite);
     CloseAudioDevice();
     CloseWindow();
     return 0;
---
+++ ARCHON_PID/src/Archer.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "raylib.h"
+
+class Archer
+{
+public:
+	Texture2D Sprite = LoadTexture("bin/Resources/Graficos/Sprites/26-267686_tortugas-ninja-png-imagenes-de-tortugas-ninja-png.png");
+};
+
