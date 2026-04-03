a94fc0bd7308ddf50e4676a22e93017110867fad
JulianJH22 <166717645+JulianJH22@users.noreply.github.com>
2026-04-03 22:46:33 +0200

    Add files via upload

---
+++ ARCHON_PID/src/ARCHON_PID.cpp
@@ -0,0 +1,29 @@
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(800, 600, "Test Raylib");
+    SetTargetFPS(60);
+    InitAudioDevice();
+    
+    Music musica = LoadMusicStream("bin/Resources/Audio/Musica/MusciaTableroFin.mp3");  // archivo en bin/
+    PlayMusicStream(musica);
+
+    while (!WindowShouldClose()) {
+        UpdateMusicStream(musica);  // necesario llamarlo cada frame
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Raylib funciona!", 300, 280, 20, DARKGRAY);
+        if (musica.stream.buffer == NULL)
+            DrawText("Error cargando musica", 300, 260, 20, RED);
+        else
+            DrawText("Musica cargada OK", 300, 260, 20, GREEN);
+        EndDrawing();
+    }
+
+    UnloadMusicStream(musica);
+    CloseAudioDevice();
+    CloseWindow();
+    return 0;
+}
